7d2ba5ddc1c044b070de5fe9ee17d677c5daf7aa
Matheus Fernandes Pinheiro <msfp@gft.com>
2025-04-22 17:47:40 -0300

    feat: remove jest

--- tests/mocks/mockProductRepository.ts
+++
@@ -1,7 +0,0 @@
-import { ProductRepositoryImpl } from '../../src/infrastructure/repositories/productRepositoryImpl';
-import { jest } from '@jest/globals';
-
-export const mockProductRepository: jest.Mocked<ProductRepositoryImpl> = {
-  findAll: jest.fn(),
-  save: jest.fn(),
-};
